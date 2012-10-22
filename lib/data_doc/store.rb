769a9f4fd9ac84be3d0a3225220721b39a5c0d4b
Alister Lee <alister.lee@dev.shortepic.com>
2012-10-22 19:33:32 +1100

    inserts rows

--- lib/data_doc/store.rb
+++ lib/data_doc/store.rb
@@ -41,22 +41,78 @@ module DataDoc
     # Define a store.
     #
     # Yields to a block calling the store for fields and other
-    # schema definition. 
+    # schema definition. Table is re-created and emptied unless 
+    # read_only, and just emptied if data_only.
     #
     def store(table_name_or_sym, &blk)
       table_name = table_name_or_sym.to_s 
       @arels[table_name] = Arel::Table.new(table_name)
-      unless @doc.read_only        
+      unless @doc.read_only
         if @doc.data_only
           @connection.delete_sql("DELETE from #{table_name}")
         else
           @connection.create_table(table_name, force: true)
         end
       end
-      self.instance_eval(&blk) if block_given?
+      begin
+        @table_name = table_name
+        self.instance_eval(&blk) if block_given?
+      ensure
+        @table_name = nil
+      end
       table_name_or_sym
     end
+        
+    #
+    # Define a string field.
+    #
+    def string(name, opts = {})
+      @connection.add_column(@table_name, name, :string, opts)
+    end
     
+    #
+    # Define an integer field.
+    #
+    def integer(name, opts = {})
+      @connection.add_column(@table_name, name, :integer, opts)
+    end
+    
+    # 
+    # Define a text field.
+    #
+    def text(name, opts = {})
+      @connection.add_column(@table_name, name, :text, opts)
+    end
+    
+    # 
+    # Define a datetime field.
+    #
+    def datetime(name, opts = {})
+      @connection.add_column(@table_name, name, :datetime, opts)
+    end    
+    
+  protected
+    
+    def insert(arel, record)
+      manager = arel.insert_manager
+      columns = record.keys.map { |k| arel[k] }
+      manager.insert(columns.zip(record.values))
+      @connection.insert(manager)
+    end
+    
+    def method_missing(name, *args, &block)
+      table_name = name.to_s
+      if @arels.has_key?(table_name)
+        if args.empty?
+          return @arels[table_name]
+        else
+          insert(@arels[table_name], *args)
+        end
+      else
+        super
+      end 
+    end
+        
   end
   
 end
--- test/test_data_doc_store.rb
+++ test/test_data_doc_store.rb
@@ -34,7 +34,7 @@ describe DataDoc::Store do
 
       describe "defining stores" do
 
-        describe "creates table" do
+        describe "create table" do
 
           before do
             @store.connection.create_table('relation', force: true)
@@ -79,17 +79,71 @@ describe DataDoc::Store do
           end
           
         end
+        
+        describe "add attributes" do
+                    
+          def check_insert(field, value)
+            @store.connection.select_value("insert into relation(#{field}) values ('#{value}')").must_be_nil
+          end
+          
+          it "adds a string attribute" do
+            @store.store('relation') do
+              string 'string'
+            end
+            check_insert('string', 'a string')
+          end
+          
+          it "adds an integer attribute" do
+            @store.store('relation') do
+              integer 'number'
+            end
+            check_insert('number', 42)
+          end
+          
+          it "adds a text attribute" do
+            @store.store('relation') do
+              text 'description'
+            end
+            check_insert('description', 'a string')            
+          end
+          
+          it "adds a datetime attribute" do
+            @store.store('relation') do
+              datetime 'timestamp'
+            end
+            check_insert('timestamp', '2012-10-12')
+          end
 
+        end
+
+        describe "alternate keys" do
+        end
+        
       end
 
+      describe "accepting rows" do
+      
+        before do
+          @store.store('relation') do
+            string 's'
+            integer 'i'
+            text 't'
+          end    
+          @store.connection.select_value("select count(1) from relation").must_equal 0
+        end
+    
+        after do
+          @store.connection.select_value("select count(1) from relation").must_equal 1
+        end
+      
+        it "should accept a row" do
+          @store.relation(s: 'a string', i: 42, t: 'a string')
+        end
+      
+      end
+    
     end
-
+    
   end
-  
-  # describe "accepting rows" do
-  #     
-  #     it "should accept rows"
-  #     
-  #  end
-  
+    
 end
